4e1f19b7cb2c54c308a8f56d922db8d4e780ab15
Erfanfooladii <arfannnfooladi@gmail.com>
2025-10-22 14:23:40 +0330

    refactor(useTelegramBackButton): streamline back button event handling
    - Updated the useTelegramBackButton hook to utilize the webApp object for back button event management.
    - Removed deprecated references and improved code clarity by simplifying the event listener setup and cleanup.
    - Ensured proper handling of the back button click event with an optional custom callback.

--- src/hooks/useTelegramBackButton.ts
+++ src/hooks/useTelegramBackButton.ts
@@ -2,33 +2,21 @@ import { useEffect } from "react";
 import { useTelegram } from "@/hooks/useTelegram";
 import { useNavigate } from "react-router-dom";
 
-/**
- * Hook to control Telegram back button behavior.
- * Automatically shows the Telegram back button and handles its click.
- *
- * @param onBack Optional custom callback to handle back button press.
- *               If not provided, it will navigate(-1) by default.
- */
 export const useTelegramBackButton = (onBack?: () => void) => {
-  const { showBackButton, hideBackButton, isInTelegram } = useTelegram();
+  const { showBackButton, webApp, hideBackButton, isInTelegram } =
+    useTelegram();
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!isInTelegram) return;
-
-    // Show Telegram back button
     showBackButton();
-
-    // Define what happens when user clicks back
     const handleBack = onBack || (() => navigate(-1));
 
-    const tg = window.Telegram?.WebApp;
-    tg?.back_button.onClick(handleBack);
+    webApp?.back_button.onClick(handleBack);
 
-    // Cleanup on unmount
     return () => {
       hideBackButton();
-      tg?.back_button.offClick(handleBack);
+      webApp?.back_button.offClick(handleBack);
     };
   }, [isInTelegram, showBackButton, hideBackButton, navigate, onBack]);
 };
